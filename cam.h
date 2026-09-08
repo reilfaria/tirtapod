32299617938e6d415f2cde3d6a5ac7dd998bcf01
Restu Firmansyah <3332230046@untirta.ac.id>
2026-09-08 21:08:02 +0700

    Enhance receiveData with timeout handling
    Added timeout handling for received data and updated logic to track last receive time.

--- cam.h
+++ cam.h
@@ -7,35 +7,39 @@ int camx_dummy = -1, camy_dummy = -1;
 int bataskanan = 350;
 int bataskiri = 280;
 
+unsigned long lastReceiveTime = 0;
+const unsigned long DATA_TIMEOUT = 300; // ms, sesuaikan nanti
+
 struct Data {
   byte id;
   byte value;
 };
 
-Data receivedData[10]; // array untuk menyimpan 9 data yang diterima
+Data receivedData[10];
 
 void receiveData(int byteCount) {
   if (byteCount > 0) {
     int dataregister = Wire1.read();
     if (dataregister == 0) {
-      if (byteCount >= sizeof(Data) * 2 + 2) { // Memastikan jumlah byte cukup untuk 8 data dan 1 id
+      if (byteCount >= sizeof(Data) * 2 + 2) {
         for (int i = 0; i < byteCount - 1; i++) {
-          receivedData[i].value = Wire1.read(); // Baca nilai data
+          receivedData[i].value = Wire1.read();
         }
 
-        // Mengurai data berdasarkan ID
         int index = 0;
+        bool gotKorban = false;
         while (index < byteCount - 1) {
           byte id = receivedData[index].value;
-          if (id == 1) { // ID 1 untuk korban
+          if (id == 1) {
             int byte1x_korban = receivedData[index + 1].value;
             int byte2x_korban = receivedData[index + 2].value;
             int byte1y_korban = receivedData[index + 3].value;
             int byte2y_korban = receivedData[index + 4].value;
             camx = byte1x_korban * 256 + byte2x_korban;
             camy = byte1y_korban * 256 + byte2y_korban;
+            gotKorban = true;
             index += 5;
-          } else if (id == 2) { // ID 2 untuk dummy
+          } else if (id == 2) {
             int byte1x_dummy = receivedData[index + 1].value;
             int byte2x_dummy = receivedData[index + 2].value;
             int byte1y_dummy = receivedData[index + 3].value;
@@ -44,16 +48,22 @@ void receiveData(int byteCount) {
             camy_dummy = byte1y_dummy * 256 + byte2y_dummy;
             index += 5;
           } else {
-            // Jika ID tidak diketahui, hentikan pengolahan
             break;
           }
         }
 
-
-      } else {
-        // Jika jumlah byte yang diterima tidak cukup, beri pesan kesalahan
+        if (gotKorban) {
+          lastReceiveTime = millis();
+        }
       }
     }
+  }
+}
+
+void checkTimeout() {
+  if (lastReceiveTime != 0 && millis() - lastReceiveTime > DATA_TIMEOUT) {
+    camx = -1;
+    camy = -1;
   }
 }
 
@@ -62,6 +72,5 @@ void setup1() {
   Wire1.setSCL(3);
   Wire1.onReceive(receiveData);
   Wire1.begin(0x03);
-
 }
 }
