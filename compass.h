1f3b3080b7b2d6fd4fea63db1c3e137d95cf475e
Restu Firmansyah <3332230046@untirta.ac.id>
2026-09-08 21:07:34 +0700

    Replace HMC5883 with BMM350 magnetometer

--- compass.h
+++ compass.h
@@ -1,7 +1,7 @@
-#include <Adafruit_Sensor.h>
-#include <Adafruit_HMC5883_U.h>
+#include <Wire.h>
+#include "DFRobot_BMM350.h"
 
-Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+DFRobot_BMM350_I2C bmm350(&Wire, I2C_ADDRESS);
 
 unsigned long int gettimecompass = 0;
 // unsigned long int gettimepitch = 0;
@@ -12,39 +12,26 @@ namespace compass {
 
 void setup() {
   Serial.begin(38400);
-  Serial.println("HMC5883 Magnetometer Test");
+  Serial.println("BMM350 Magnetometer Test");
   Serial.println("");
 
   /* Initialise the sensor */
-  if (!mag.begin()) {
-    /* There was a problem detecting the HMC5883 ... check your connections */
-    Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
+  if (bmm350.begin()) {
+    /* There was a problem detecting the BMM350 ... check your connections */
+    Serial.println("Ooops, no BMM350 detected ... Check your wiring!");
     while (1)
       ;
   }
+
+  bmm350.setOperationMode(eBmm350NormalMode);
+  bmm350.setPresetMode(BMM350_PRESETMODE_HIGHACCURACY, BMM350_DATA_RATE_25HZ);
+  bmm350.setMeasurementXYZ();
 }
 
 float heading() {
   if (millis() - gettimecompass >= 200) {
     gettimecompass = millis();
-
-    sensors_event_t event;
-    mag.getEvent(&event);
-
-    float heading = atan2(event.magnetic.y, event.magnetic.x);
-    float declinationAngle = 0.00755;
-    heading += declinationAngle;
-
-    // Correct for when signs are reversed.
-    if (heading < 0)
-      heading += 2 * PI;
-
-    // Check for wrap due to addition of declination.
-    if (heading > 2 * PI)
-      heading -= 2 * PI;
-
-    // Convert radians to degrees for readability.
-    headingDegrees = heading * 180 / M_PI;
+    headingDegrees = bmm350.getCompassDegree();
   }
   return headingDegrees;
 }
@@ -52,11 +39,9 @@ float heading() {
 // float getpitch() {
 //   if (millis() - gettimepitch >= 250) {
 //     gettimepitch = millis();
-
-//     sensors_event_t event;
-//     mag.getEvent(&event);
-
-//     pitch = event.magnetic.x;
+//
+//     sBmm350MagData_t magData = bmm350.getGeomagneticData();
+//     pitch = magData.x;
 //   }
 //   return pitch;
 // }
